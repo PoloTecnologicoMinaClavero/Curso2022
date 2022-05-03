5bb8468c857f42beb97cb98d61b8bcafa4e9a7eb
Eugenio Serrano <eugenioserrano@msn.com>
2022-05-03 19:55:24 -0300

    Subimos el ejemplo de la primera parte de la clase 3

---
+++ Clase3/Saludo/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// Borra la pantalla
+Console.Clear();
+
+// Pedimos al usuario que introduzca su nombre
+Console.WriteLine("Hola, Ingrese su nombre:");
+
+// Leemos el nombre del usuario
+string nombre = Console.ReadLine();
+
+// Pedimos al usuario que introduzca su apellido
+Console.WriteLine("Ahora por favor ingrese su apellido:");
+
+string apellido = Console.ReadLine();
+
+// Mostramos el nombre
+Console.Write("Hola " + nombre);
+Console.WriteLine(", este es mi segundo programa.");
+
+Console.WriteLine("Pulse una tecla para finalizar.");
+Console.ReadKey();
+
