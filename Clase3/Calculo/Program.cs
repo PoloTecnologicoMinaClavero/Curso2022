4ae67875b1d002f27c0afbae9a09f6f83a5c0b75
Eugenio Serrano <eugenioserrano@msn.com>
2022-05-03 20:29:31 -0300

    Subimos el ejemplo de la clase 3

--- Clase3/Calculo/Program.cs
+++ Clase3/Calculo/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+
+Console.WriteLine("Este programa calcula la superficie de un rectangulo");
+Console.WriteLine();
+
+//decimal v = decimal.Parse("222");
+
+//string baseRectanguloTexto = Console.ReadLine();
+//double baseRectangulo = double.Parse(baseRectanguloTexto);
+
+Console.WriteLine("Ingrese la base del rectangulo");
+double baseRectangulo = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la altura del rectangulo");
+double alturaRectangulo = double.Parse(Console.ReadLine());
+
+double superficieRectangulo =
+    baseRectangulo * alturaRectangulo;
+
+Console.Write("La superficie del rectangulo es: ");
+Console.WriteLine(superficieRectangulo);
+
+bool esMayorADiez = superficieRectangulo > 10;
+
+if (esMayorADiez)
+{
+    // Mostrar mensaje solamente si es mayor a diez
+    Console.WriteLine("La superficie del rectangulo es mayor a 10");
+}
+
+//string cualquiercosa = "hola";
+
+Console.ReadKey();
