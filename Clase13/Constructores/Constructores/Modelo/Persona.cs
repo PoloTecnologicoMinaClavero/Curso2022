52e20c23d860b6a4691f8821e1ab929d235a1b41
eze-etchecoin <eze.etchecoin.92@gmail.com>
2022-06-07 22:30:22 -0300

    subimos archivos de la clase 13

---
+++ Clase13/Constructores/Constructores/Modelo/Persona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructores.Modelo
+{
+    internal class Persona
+    {   
+        //Propiedades
+        public string NombreCompleto { get; set; }
+        public string NumeroDocumento { get; set; }
+        public int Edad { get; set; }
+        public bool EstaCasado { get; set; }
+
+        //Constructor
+        public Persona()
+        {
+            Console.WriteLine("Se ha instanciado una nueva persona.");
+        }
+
+        public Persona(string nombreCompleto, string numeroDocumento, int edad, bool estaCasado)
+        {
+            NombreCompleto = nombreCompleto;
+            NumeroDocumento = numeroDocumento;
+            Edad = edad;
+            EstaCasado = estaCasado;
+        }
+
+        //Métodos o funciones
+        public string ObtenerSaludo()
+        {
+            return $"Hola! Soy {NombreCompleto}";
+        }
+
+        public string ObtenerSaludo(string nombrePersonaAQuienSaludar)
+        {
+            return $"Hola {nombrePersonaAQuienSaludar}, soy {NombreCompleto}";
+        }
+    }
+}
---
+++ Clase13/Constructores/Constructores/Program.cs
@@ -0,0 +1,11 @@
+﻿using Constructores.Modelo;
+
+var persona1 = new Persona("Ezequiel Etchecoin", "12312", 30, true);
+
+Console.WriteLine(persona1.ObtenerSaludo());
+Console.WriteLine(persona1.ObtenerSaludo("Carlos"));
+
+var persona2 = new Persona { NombreCompleto = "Carlos Moscoso" };
+
+//Esta clase también se incorpora desde el namespace Constructores.Modelo
+var miOcupacion = new Ocupacion();
