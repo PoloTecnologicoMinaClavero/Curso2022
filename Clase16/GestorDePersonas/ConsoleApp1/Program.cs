c4c8d61e077cee5cee4c5abdc137746aed4db2b7
eze-etchecoin <eze.etchecoin.92@gmail.com>
2022-06-16 20:53:08 -0300

    subimos los archivos de la clase 16

---
+++ Clase16/GestorDePersonas/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp1.Modelo;
+using ConsoleApp1.Repositorio;
+
+//Gestor de personas trabajadoras (empleados, desempleados, jubilados)
+//Insertar personas en una base de datos, mostrar datos de una determinada persona o
+//de todas las personas.
+
+var repositorio = new RepositorioDePersonas();
+
+//Le pido al usuario los datos de una persona para agregar
+//El usuario debería elegir el tipo de persona a agregar
+var usuarioNombre = "Ezequiel";
+var usuarioApellido = "Etchecoin";
+
+repositorio.Insertar(new Desemplado
+{
+    Nombre = usuarioNombre,
+    Apellido = usuarioApellido
+});
