67e7c264d6dab640b7d804dedc9ce729c067f29f
Eugenio Serrano <eugenioserrano@msn.com>
2022-06-21 20:39:14 -0300

    Subimos el ejemplo de polimorfismo

---
+++ Clase17/Billetera/Billetera/IBilletera.cs
@@ -0,0 +1,18 @@
+﻿using Billetera.Version2;
+
+namespace Billetera
+{
+    public interface IBilletera
+    {
+        int BilletesDe10 { get; set; }
+        int BilletesDe100 { get; set; }
+        int BilletesDe1000 { get; set; }
+        int BilletesDe20 { get; set; }
+        int BilletesDe200 { get; set; }
+        int BilletesDe50 { get; set; }
+        int BilletesDe500 { get; set; }
+
+        IBilletera Combinar(IBilletera b);
+        decimal Total();
+    }
+}
---
+++ Clase17/Billetera/Billetera/Program.cs
@@ -0,0 +1,30 @@
+﻿using Billetera;
+
+//var b1 = new Billetera.Billetera();
+//var b2 = new Billetera.Billetera();
+//b1.BilletesDe100 = 1;
+//b2.BilletesDe1000 = 1;
+//Console.WriteLine(b1.Total().ToString());
+//Console.WriteLine(b2.Total().ToString());
+//var b3 = b1.Combinar(b2);
+//Console.WriteLine(b3.Total().ToString());
+
+
+//var bc1 = new Billetera.Version2.BilleteraCarlos();
+//var bc2 = new Billetera.Version2.BilleteraCarlos();
+//bc1.BilletesDe100 = 1;
+//bc2.BilletesDe1000 = 1;
+//Console.WriteLine(bc1.Total().ToString());
+//Console.WriteLine(bc2.Total().ToString());
+//var bc3 = bc1.Combinar(bc2);
+//Console.WriteLine(bc3.Total().ToString());
+
+
+var b1 = new Billetera.Billetera();
+var bc2 = new Billetera.Version2.BilleteraCarlos();
+b1.BilletesDe100 = 1;
+bc2.BilletesDe1000 = 1;
+Console.WriteLine(b1.Total().ToString());
+Console.WriteLine(bc2.Total().ToString());
+IBilletera bc3 = b1.Combinar(bc2);
+Console.WriteLine(bc3.Total().ToString());
