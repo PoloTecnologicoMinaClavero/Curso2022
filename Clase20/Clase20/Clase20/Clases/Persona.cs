926db5ef8552fc2bd99e9ac512da034fa2c61eeb
Eugenio Serrano <eugenioserrano@msn.com>
2022-06-30 19:38:42 -0300

    Subimos primera parte clase 20

---
+++ Clase20/Clase20/Clase20/Clases/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase20.Clases
+{
+    public partial class Persona
+    {
+        public Persona()
+        {
+            Nombre = "";
+            Apellido = "";
+        }
+
+        public string Nombre { get; set; }
+
+        public partial int CaludarEdad();
+    }
+}
---
+++ Clase20/Clase20/Clase20/Program.cs
@@ -0,0 +1,15 @@
+﻿using Clase20.Clases;
+
+var p = new Persona
+{
+    Nombre = "Juan",
+    Apellido = "Perez"
+};
+
+
+var p2 = new Persona();
+p2.Nombre = "Juan";
+p2.Apellido = "Perez";
+
+
+var miArray = new int[] { 1, 2, 3, 4, 5 };
