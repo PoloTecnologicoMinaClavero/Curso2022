79f56fa13df115efe6c02666f7d954c2f2c84937
Eugenio Serrano <eugenioserrano@msn.com>
2022-06-30 21:00:55 -0300

    subimos un cambiecito

--- Clase20/WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ Clase20/WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Salir";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // checkBox1
             // 
--- Clase20/WinFormsApp1/WinFormsApp1/Form1.cs
+++ Clase20/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -7,6 +7,9 @@ namespace WinFormsApp1
             InitializeComponent();
         }
 
-        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
