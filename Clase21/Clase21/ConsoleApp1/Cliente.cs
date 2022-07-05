c702797162ddafa3e19ca215545e36a95669b7e9
Eugenio Serrano <eugenioserrano@msn.com>
2022-07-05 20:12:30 -0300

    Subimos los ejemplos de la clase 21

---
+++ Clase21/Clase21/ConsoleApp1/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Cliente
+    {
+        public string Apellido;
+        public string Nombre;
+    }
+}
