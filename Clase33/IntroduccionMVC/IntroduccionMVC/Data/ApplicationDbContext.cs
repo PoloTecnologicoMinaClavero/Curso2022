ff770638e116318ca22cdf5ca2f49c785db50d5c
Ruben Benegas <ruben.benegas@incluit.com>
2022-08-30 22:40:01 -0300

    Subimos proyecto de ejemplo de MVC

---
+++ Clase33/IntroduccionMVC/IntroduccionMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroduccionMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public ApplicationDbContext()
+        {
+
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=aspnet-IntroduccionMVC-E925FED6-ECB3-4836-977B-02C4DB56EBAF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+    }
+}
---
+++ Clase33/IntroduccionMVC/IntroduccionMVC/Models/UserViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace IntroduccionMVC.Models
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
