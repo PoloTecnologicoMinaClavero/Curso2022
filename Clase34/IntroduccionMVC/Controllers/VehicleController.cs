f18bfa5b73e7e25557f36135f1327475105ceeea
eze-etchecoin <eze.etchecoin.92@gmail.com>
2022-09-01 20:39:45 -0300

    subimos archivos de la clase 34

---
+++ Clase34/IntroduccionMVC/Controllers/VehicleController.cs
@@ -0,0 +1,52 @@
+﻿using IntroduccionMVC.Data;
+using IntroduccionMVC.Data.Entities;
+using IntroduccionMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace IntroduccionMVC.Controllers
+{
+    public class VehicleController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        
+        public VehicleController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        public IActionResult Index()
+        {
+            var vehiculos = _dbContext.Vehiculos.ToList();
+
+            //var model = new VehicleListViewModel
+            //{
+            //    Vehiculos = new List<VehicleListModel>()
+            //};
+
+            //foreach(var vehiculo in vehiculos)
+            //{
+            //    model.Vehiculos.Add(new VehicleListModel
+            //    {
+            //        Id = vehiculo.Id,
+            //        Dominio = vehiculo.Dominio,
+            //        AnioFabricacion = vehiculo.AnioFabricacion
+            //    });
+            //}
+
+            var model = new VehicleListViewModel
+            {
+                Vehiculos = vehiculos
+                    .Select(v => new VehicleListModel
+                    {
+                        Id = v.Id,
+                        Dominio = v.Dominio,
+                        AnioFabricacion = v.AnioFabricacion
+                    })
+                    .ToList()
+            };
+            
+            return View("List", model);
+        }
+    }
+}
---
+++ Clase34/IntroduccionMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using IntroduccionMVC.Data.Entities;
+using IntroduccionMVC.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroduccionMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vehiculo> Vehiculos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=EZE-INCLUIT\\SQLEXPRESS2012;Initial Catalog=aspnet-IntroduccionMVC-E925FED6-ECB3-4836-977B-02C4DB56EBAF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+    }
+}
