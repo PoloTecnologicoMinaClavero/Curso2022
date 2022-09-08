b851b43bb98fe2884dbfdc290dc57a03abb4a945
carlosmartin667 <46163584+carlosmartin667@users.noreply.github.com>
2022-09-08 20:33:41 -0300

    agregamos clase 36

---
+++ Clase36/IntroduccionMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using IntroduccionMVC.Data.Entities;
+using IntroduccionMVC.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroduccionMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vehiculo> Vehiculos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=CARLOS-CORNEJO;Initial Catalog=IntroduccionMVC;Integrated Security=True");
+            }
+        }
+    }
+}
