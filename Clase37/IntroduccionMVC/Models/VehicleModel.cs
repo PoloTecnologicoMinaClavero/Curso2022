399f005a6039ad60aa181cbced36217d46f69f18
Ruben Benegas <ruben.benegas@incluit.com>
2022-09-13 20:34:47 -0300

    Subimos nuestro primer ABM!!!

---
+++ Clase37/IntroduccionMVC/Models/VehicleModel.cs
@@ -0,0 +1,11 @@
+﻿namespace IntroduccionMVC.Models
+{
+    public class VehicleModel
+    {
+        public int Id { get; set; }
+        public string Dominio { get; set; }
+        public string NumeroChasis { get; set; }
+        public string Propietario { get; set; }
+        public int AnioFabricacion { get; set; }
+    }
+}
