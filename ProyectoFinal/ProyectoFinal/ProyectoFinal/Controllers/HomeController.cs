b147bcc75ec37ca151dffdd2425c87a5d29e9970
jmateosincluit <mateos.joaquin@gmail.com>
2022-10-11 20:27:00 -0300

    clase 44

--- ProyectoFinal/ProyectoFinal/ProyectoFinal/Controllers/HomeController.cs
+++ ProyectoFinal/ProyectoFinal/ProyectoFinal/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProyectoFinal.Models;
+using ProyectoFinal.Rules;
 using System.Diagnostics;
 
 namespace ProyectoFinal.Controllers
@@ -17,11 +18,21 @@ namespace ProyectoFinal.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Suerte()
+        {
+            var rule = new PublicacionRule();
+            var post = rule.GetOnePostRandom();
+            return View(post);
+        }
+        public IActionResult AcercaDe()
         {
             return View();
         }
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ ProyectoFinal/ProyectoFinal/ProyectoFinal/Models/Publicacion.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoFinal.Models
+{
+    public class Publicacion
+    {
+        public string Titulo { get; set; } = string.Empty;
+        public string SubTitulo { get; set; } = string.Empty;
+        public string Creador { get; set; } = string.Empty;
+        public string Cuerpo { get; set; } = string.Empty;
+        public DateTime Creacion { get; set; } = DateTime.Now;
+        public string Imagen { get; set; } = string.Empty;
+    }
+}
