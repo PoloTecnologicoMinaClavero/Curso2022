dcd694cbadfa44c9d6f9ee699e6b658d2d70065d
Eugenio Serrano <eugenioserrano@msn.com>
2022-10-13 19:23:59 -0300

    Creamos el proyecto de base de datos y la tabla publicacion

---
+++ ProyectoFinal/ProyectoFinal/BlogDatabase/Publicacion.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Publicacion](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Titulo] [varchar](100) NOT NULL,
+	[Subtitulo] [varbinary](100) NOT NULL,
+	[Creador] [varchar](50) NOT NULL,
+	[Cuerpo] [varchar](max) NOT NULL,
+	[Creacion] [date] NOT NULL,
+	[Imagen] [varchar](200) NOT NULL,
+ CONSTRAINT [PK_Publicacion] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
