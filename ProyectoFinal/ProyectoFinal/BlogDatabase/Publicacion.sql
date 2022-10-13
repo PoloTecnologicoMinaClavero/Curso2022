e45515bdb68ae862b1a4f8e37d2bd8c1f5109924
Eugenio Serrano <eugenioserrano@msn.com>
2022-10-13 19:46:33 -0300

    Subimos lo hecho en la primera parte de la clase 45

--- ProyectoFinal/ProyectoFinal/BlogDatabase/Publicacion.sql
+++ ProyectoFinal/ProyectoFinal/BlogDatabase/Publicacion.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Publicacion](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[Titulo] [varchar](100) NOT NULL,
-	[Subtitulo] [varbinary](100) NOT NULL,
+	[Subtitulo] [varchar](100) NOT NULL,
 	[Creador] [varchar](50) NOT NULL,
 	[Cuerpo] [varchar](max) NOT NULL,
 	[Creacion] [date] NOT NULL,
